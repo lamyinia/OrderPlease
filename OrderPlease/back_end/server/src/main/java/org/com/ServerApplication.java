8977f21f065fba89f401d984c5a143f702da940d
lamyinia <142591822+lamyinia@users.noreply.github.com>
2025-06-20 00:52:56 +0800

    项目创建，依赖导入

---
+++ OrderPlease/back_end/server/src/main/java/org/com/ServerApplication.java
@@ -0,0 +1,14 @@
+package org.com;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class ServerApplication {
+    public static void main(String[] args) {
+        log.info("spring server 启动成功");
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
