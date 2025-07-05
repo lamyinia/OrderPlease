8ba5f5517043479793aab2620d4b34843f3b4188
lamyinia <142591822+lamyinia@users.noreply.github.com>
2025-07-05 10:38:49 +0800

    所有pojo、controller、mapper、service、serviceImpl声明

--- OrderPlease/back_end/server/src/main/java/org/com/controller/admin/CategoryController.java
+++ OrderPlease/back_end/server/src/main/java/org/com/controller/admin/CategoryController.java
@@ -5,11 +5,14 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.com.dto.CategoryDTO;
 import org.com.dto.CategoryPageQueryDTO;
+import org.com.entity.Category;
 import org.com.result.PageResult;
 import org.com.result.Result;
 import org.com.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api("菜品套餐分类接口")
 @Slf4j
@@ -19,23 +22,24 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    // TODO
+    @GetMapping("/list")
+    @ApiOperation("类型分页")
+    public Result<List<Category>> selectByType(Integer type){
+        List<Category> list = categoryService.selectByType(type);
+        return null;
+    }
+
+    @GetMapping("/page")
+    @ApiOperation("普通分页")
+    public Result<PageResult> selectPage(CategoryPageQueryDTO categoryPageQueryDTO){
+        return Result.success(categoryService.selectPage(categoryPageQueryDTO));
+    }
+
     @PostMapping
     @ApiOperation("新增")
     public Result<String> addCategory(@RequestBody CategoryDTO categoryDTO){
         categoryService.addCategory(categoryDTO);
-        return Result.success();
-    }
-
-    @GetMapping("/page")
-    @ApiOperation("分页")
-    public Result<PageResult> selectPage(CategoryPageQueryDTO categoryPageQueryDTO){
-        return categoryService.selectPage(categoryPageQueryDTO);
-    }
-
-    @PutMapping
-    @ApiOperation("修改")
-    public Result<String> update(@RequestBody CategoryDTO categoryDTO){
-        categoryService.updateCategory(categoryDTO);
         return Result.success();
     }
 
@@ -46,11 +50,17 @@ public class CategoryController {
         return Result.success();
     }
 
-    // TODO
-    public Result<String> turnState(){
-        return null;
+    @PutMapping
+    @ApiOperation("修改")
+    public Result<String> update(@RequestBody CategoryDTO categoryDTO){
+        categoryService.updateCategory(categoryDTO);
+        return Result.success();
     }
-    public Result<String> selectByType(){
-        return null;
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("改变状态")
+    public Result<String> turnState(@PathVariable("status") Integer status, Long id){
+        categoryService.turnState(status, id);
+        return Result.success();
     }
 }
